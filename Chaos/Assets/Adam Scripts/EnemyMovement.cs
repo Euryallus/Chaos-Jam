959d5d238977aa58ee3251e992078a631055b6c1
Adamjveasey <61789322+Adamjveasey@users.noreply.github.com>
2021-02-19 13:55:04 +0000

    Enemy movement towards target position

---
+++ Chaos/Assets/Adam Scripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public GameObject targetObject;
+    public float speed = 3;
+
+    private float step;
+    private Vector2 targetPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        step = speed * Time.deltaTime;
+
+        targetPosition = targetObject.transform.position;
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, step);
+    }
+}
