9694993dc7a63611aa7124828bf82c17a0d846a3
Adamjveasey <61789322+Adamjveasey@users.noreply.github.com>
2021-02-19 20:23:51 +0000

    Bat UwU

--- Chaos/Assets/Adam Scripts/EnemyMovement.cs
+++ Chaos/Assets/Adam Scripts/EnemyMovement.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public GameObject targetObject;
-    public float speed = 3;
+    public enum States
+    {
+        active,
+        idle,
+        patrol
+    }
+    
+    public float speed = 1.5;
 
-    private float step;
-    private Vector2 targetPosition;
+    private GameObject  playerObject;
+    private float       step;
+    private Vector2     targetPosition;
+    private Vector3     targetDirection;
+    private States      state = States.idle;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        state = States.active;
+
+        playerObject = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        step = speed * Time.deltaTime;
+        if (state == States.active)
+        {
+            step = speed * Time.deltaTime;
 
-        targetPosition = targetObject.transform.position;
+            targetPosition = playerObject.transform.position;
 
-        transform.position = Vector2.MoveTowards(transform.position, targetPosition, step);
+            targetDirection = targetPosition - new Vector2(transform.position.x, transform.position.y);
+
+            GetComponent<Animator>().SetFloat("TurnSide", targetDirection.x); 
+            
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, step);
+        }
     }
 }
