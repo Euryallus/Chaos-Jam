a117866dfe6d189a2993e23022ad2fc1b9e73ff6
Hugo Bailey <hugobailey09@gmail.com>
2021-02-21 22:59:10 +0000

    restart

---
+++ Chaos/Assets/Hugo Scripts/LevelController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject centreRoom;
+
+    [SerializeField]
+    GameObject[] enemies;
+
+    [SerializeField]
+    private bool complete = false;
+
+    public void Restart()
+    {
+        GameObject[] rooms = GameObject.FindGameObjectsWithTag("Room");
+        Destroy(GameObject.FindGameObjectWithTag("SpawnRoom"));
+
+        for (int i = 0; i < rooms.Length; i++)
+        {
+            Destroy(rooms[i]);
+        }
+
+        GameObject newRoom = Instantiate(centreRoom, position: new Vector3(0, 0, 0), rotation: new Quaternion(0,0,0,0));
+
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        complete = false;
+    }
+
+    public bool HasFinishedLevel()
+    {
+        return complete;
+    }
+
+    private void Update()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        if(enemies.Length == 0)
+        {
+            complete = true;
+        }
+        else
+        {
+            complete = false;
+        }
+    }
+}
