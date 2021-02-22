453d20687ae23a47eb49324e63125f5c464c18ef
SetumupJoeDev <61789327+SetumupJoeDev@users.noreply.github.com>
2021-02-22 00:33:39 +0000

    Changed boomerangs to break pots

--- Chaos/Assets/James Scripts/Player/Scripts/BoomerangController.cs
+++ Chaos/Assets/James Scripts/Player/Scripts/BoomerangController.cs
@@ -114,19 +114,22 @@ public class BoomerangController : MonoBehaviour
     private bool attackEnemies( )
     {
 
-        if( m_currentTargetsColliders[m_currentTargetIndex].gameObject.GetComponent<EnemyHealthManager>() == null)
-        {
-            m_currentTargetIndex++;
-        }
-
         transform.position = Vector2.MoveTowards( transform.position, m_currentTargetsColliders[m_currentTargetIndex].gameObject.transform.position, m_moveSpeed * Time.deltaTime );
 
         if( Vector2.Distance( transform.position, m_currentTargetsColliders[m_currentTargetIndex].gameObject.transform.position ) <= 0 )
         {
-            // Accesses the enemy's script and reduces their health
-            if( m_currentTargetsColliders[m_currentTargetIndex].gameObject.GetComponent<EnemyHealthManager>( ).TakeDamage( ) )
+
+            if( m_currentTargetsColliders[m_currentTargetIndex].gameObject.GetComponent<EnemyHealthManager>() != null )
             {
-                m_playerObject.GetComponent<XPManager>( ).gainXP( );
+                // Accesses the enemy's script and reduces their health
+                if (m_currentTargetsColliders[m_currentTargetIndex].gameObject.GetComponent<EnemyHealthManager>().TakeDamage())
+                {
+                    m_playerObject.GetComponent<XPManager>().gainXP();
+                }
+            }
+            else if( m_currentTargetsColliders[m_currentTargetIndex].gameObject.GetComponent<BreakableObject>() != null)
+            {
+                m_currentTargetsColliders[m_currentTargetIndex].gameObject.GetComponent<BreakableObject>().breakOpen();
             }
 
             // Incremenets the target index to move on to the next enemy in the array
